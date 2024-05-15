41b5b172ff4f79445aa696c015763bfcfdea622d
md.nurnabimiah <diunayonahmed@gmail.com>
2024-05-15 21:54:57 +0600

    error solve: version error solve

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+//version problem solve
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
